08f024a48d15e954a509567d906a08e31ffc6e55
Nicholas E. Rabenau <nerab@gmx.net>
2012-07-13 23:44:42 +0200

    Mark tag tests as incomplete

--- test/unit/test_tag_repository.rb
+++ test/unit/test_tag_repository.rb
@@ -5,37 +5,38 @@ class TestTagRepository < Test::Unit::TestCase
   include TaskWarrior::Test::Fixtures
 
   def setup
+    skip "Not done yet"
     super
 
     @tags = TagRepository.new
-    assert_equal(0, @repo.all.size)
+    assert_equal(0, @tags.all.size)
     cmd = Commands::Import.new(File.new(fixture('party_taxes.json')))
     cmd.run
-    assert_equal(8, @repo.all.size)
+    assert_equal(8, @tags.all.size)
   end
 
   def test_tags
     tags = @tags.all
     assert_not_nil(tags)
     assert_equal(2, tags.size)
-    assert(tags.include?(@repo.tag('finance')))
-    assert(tags.include?(@repo.tag('mall')))
+    assert(tags.include?(@tags.tag('finance')))
+    assert(tags.include?(@tags.tag('mall')))
   end
 
   def test_tasks_of_tag_finance
-    finance = @repo.tag('finance')
+    finance = @tags.tag('finance')
     assert_not_nil(finance)
     assert_equal(2, finance.tasks.size)
   end
 
   def test_tasks_of_tag_mall
-    mall = @repo.tag('mall')
+    mall = @tags.tag('mall')
     assert_not_nil(mall)
     assert_equal(3, mall.tasks.size)
   end
 
   def test_equality
-    t1 = @repo['b587f364-c68e-4438-b4d6-f2af6ad62518']
+    t1 = @tags['b587f364-c68e-4438-b4d6-f2af6ad62518']
     t2 = t1.dup
     assert_not_equal(t1.object_id, t2.object_id)
 
