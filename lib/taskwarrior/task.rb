fb7e1e2141f1bdc748318be738a3394d8125e90c
Nicholas E. Rabenau <nerab@gmx.net>
2013-07-21 17:42:04 +0200

    Use SecureRandom.uuid instead of an external binary

--- lib/taskwarrior/task.rb
+++ lib/taskwarrior/task.rb
@@ -2,7 +2,7 @@ module TaskWarrior
   class Task
     include TaskWarrior::Attributes
 
-    attributes :description, :id, :entry, :status, :uuid, :project, :dependencies, :parent, :children, 
+    attributes :description, :id, :entry, :status, :uuid, :project, :dependencies, :parent, :children,
                :priority, :tags, :annotations, :start_at, :wait_at, :end_at, :due_at
 
     include ActiveModel::Validations
@@ -10,17 +10,17 @@ module TaskWarrior
     validates :description, :id, :entry, :status, :uuid, :presence => true
 
     validates :id, :numericality => {
-      :only_integer => true, 
+      :only_integer => true,
       :greater_than => 0
     }
 
-    validates :uuid, :format => {   
+    validates :uuid, :format => {
       :with    => /[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/,
       :message => "'%{value}' does not match the expected format of a UUID"
     }
 
-    validates :status, :inclusion => {     
-      :in      => [:pending, :waiting, :complete], 
+    validates :status, :inclusion => {
+      :in      => [:pending, :waiting, :complete],
       :message => "%{value} is not a valid status"
     }
 
@@ -41,9 +41,7 @@ module TaskWarrior
       @children = []
       @tags = []
       @annotations = []
-
-      # http://www.ruby-forum.com/topic/164078#722181
-      @uuid = %x[uuidgen].strip
+      @uuid = SecureRandom.uuid
     end
 
     def to_s
